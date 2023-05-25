ebee8c3be8d4cdd2f5ca5740ccf5d36b813cd1f2
Elijah Mooring <vehmloewff@gmail.com>
2023-05-24 17:53:26 -0700

    ref: move devops into their own dir

---
+++ devops/deps.ts
@@ -0,0 +1,4 @@
+export * as dtils from 'https://deno.land/x/dtils@2.0.0-beta.5/mod.ts'
+
+// TODO: bundler here is deprecated... update to another source
+export * as bundler from 'https://code.jikno.com/devops@0.1.0/bundle/mod.ts'
--- devops/main.ts
+++ devops/main.ts
@@ -1,12 +1,5 @@
-import { bundleWatch } from 'https://code.jikno.com/devops@0.1.0/bundle/mod.ts'
-import { env } from 'https://code.jikno.com/devops@next/lib/mod.ts'
-import { shouldReload } from 'https://code.jikno.com/devops@next/cli/mod.ts'
-import { sh } from 'https://deno.land/x/dtils@1.6.3/lib/sh.ts'
-// import * as dtils from 'https://deno.land/x/dtils@2.0.0-beta.5/mod.ts'
-import * as porter from 'https://deno.land/x/port@1.0.0/mod.ts'
-import { open } from 'https://deno.land/x/open@v0.0.6/index.ts'
+import { bundler, dtils } from './deps.ts'
 
-/** @asset [darwin] https:// */
 export async function example(args: string[]) {
 	const validExamples = await readDir('examples').then((entries) => entries.filter((entry) => entry.isDirectory).map((entry) => entry.name))
 
@@ -16,11 +9,11 @@ export async function example(args: string[]) {
 		throw new Error(`Example "${example}" does not exist. Valid examples are ${validExamples.join(', ')}`)
 	}
 
-	await bundleWatch({
+	await bundler.bundleWatch({
 		abortSignal: new AbortController().signal,
 		entryPath: `examples/${example}/main.ts`,
-		env: { ENV: env },
-		shouldRefetchRemote: shouldReload,
+		env: { ENV: dtils.getEnv() },
+		shouldRefetchRemote: dtils.getShouldReloadDeps(),
 		onBundleError(error) {
 			console.error(error)
 		},
@@ -34,16 +27,8 @@ export async function example(args: string[]) {
 		},
 	})
 
-	const port = await porter.getAvailablePort()
-	if (!port) throw new Error('Could not find an available port')
-
 	// Serve up the current example
-	await Promise.all([
-		sh('deno run -A --watch examples/serve.ts', {
-			env: { PORT: `${port}` },
-		}),
-		// setTimeout(() => open(`http://localhost:${port}`)),
-	])
+	await dtils.sh('deno run -A --watch examples/serve.ts')
 }
 
 async function readDir(dir: string) {
